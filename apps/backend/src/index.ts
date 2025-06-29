f72d10eb9c51c1373779aebb30d04f8b72d5210e
ansh-pachauri <yepitsansh2@gmail.com>
2025-06-29 12:04:34 +0530

    first commit

---
+++ apps/backend/src/index.ts
@@ -0,0 +1,12 @@
+import express, {Request, Response} from "express";
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello, world!");
+}); 
+
+app.listen(3001, () => {
+  console.log("Server is running on port 3001");
+});
